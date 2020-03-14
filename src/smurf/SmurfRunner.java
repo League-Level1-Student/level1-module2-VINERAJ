1d2686abb0dbd0a163bf7c5d247a549ef5571b7f
Vinay <vinay.e.rajagopalan@gmail.com>
2020-03-14 11:45:37 -0700

    on tea maker
    :fr: :100:

--- src/smurf/SmurfRunner.java
+++ src/smurf/SmurfRunner.java
@@ -5,7 +5,15 @@ public static void main(String[] args) {
 	Smurf handy = new Smurf("Handy Smurf");
 	System.out.println(handy.getName());
 	handy.eat();
-	handy.getHatColor();
-	handy.isGirlOrBoy();
+	System.out.println(handy.getHatColor());
+	System.out.println(	handy.isGirlOrBoy());
+	Smurf papaSmurf = new Smurf("Papa Smurf");
+	System.out.println(papaSmurf.getName());
+	System.out.println(papaSmurf.getHatColor());
+	System.out.println(papaSmurf.isGirlOrBoy());
+	Smurf smurfette = new Smurf("Smurfette");
+	System.out.println(smurfette.getName());
+	System.out.println(smurfette.getHatColor());
+	System.out.println(smurfette.isGirlOrBoy());
 }
 }
